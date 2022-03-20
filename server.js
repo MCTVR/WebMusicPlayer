3eaa04cc75873e664787b9199c5d6461e146def9
MCTVR <69135058+MCTVR@users.noreply.github.com>
2022-03-21 00:52:18 +0800

    v1

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const https = require("https");
+const compression = require("compression");
+const fs = require("fs");
+
+const env = require("./env.json");
+
+const app = express();
+app.use(compression());
+app.use(express.static("static"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+https.globalAgent.maxSockets = Infinity;
+
+const options = {
+    key: fs.readFileSync("./certs/server.key"),
+    cert: fs.readFileSync("./certs/server.cert"),
+};
+
+https.createServer(options, app)
+    .listen(env.SERVER_PORT, () => {
+        console.log(`Server is listening on port ${env.SERVER_PORT}`);
+    });
