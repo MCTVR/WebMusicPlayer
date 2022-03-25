ac7732ec547f99a7b3d170398d7cb11827ec98a7
MCTVR <69135058+MCTVR@users.noreply.github.com>
2022-03-25 16:34:54 +0800

    some logos

---
+++ static/assets/Img2Webp.py
@@ -0,0 +1,21 @@
+from re import I
+import cv2
+from PIL import Image
+import numpy as np
+
+def makeWebp(img: str, sizes: list):
+    imgName = img.split(".")[0]
+    img = cv2.imread(img, cv2.IMREAD_UNCHANGED)
+    for size in sizes:
+        img1 = cv2.resize(img, (size, size), interpolation = cv2.INTER_AREA)
+        cv2.imwrite(f"{imgName}@{size}.webp", img1, [cv2.IMWRITE_WEBP_QUALITY, 100])
+
+def makeIco(img: str, sizes: list):
+    imgName = img.split(".")[0]
+    img = Image.open(img)
+    for size in sizes:
+        img1 = img.resize((size, size), Image.ANTIALIAS)
+        img1.save(f"{imgName}@{size}.ico")
+
+#makeWebp("icon.png", [96, 128, 256, 512, 1024])
+makeIco("icon.png", [32, 64, 128, 256])
