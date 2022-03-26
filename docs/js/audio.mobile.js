353d9944e64a7c352badbe31aaf8105b494e6086
MCTVR <69135058+MCTVR@users.noreply.github.com>
2022-03-27 00:13:23 +0800

    fix bug maybe

--- docs/js/audio.mobile.js
+++ docs/js/audio.mobile.js
@@ -21,38 +21,36 @@ let audioElement = new Audio;
 let times = 0;
 
 function audio(file, trackCtl=false) {
-    if (isUnlocked === true) {
         
-        if (times > 1) {
-            times = 0;
-            audioElement.pause();
-            URL.revokeObjectURL(audioElement.src);
-            clearInterval(progressInterval);
-            clearInterval(progressBarInterval);
-            audioElement.src = "";
-        }
-        
-        (async () => {
-            times += 1;
-            URL.revokeObjectURL(audioElement.src);
-            try {
-
-                let blob = new Blob([ await file.arrayBuffer()], { type: file.type });
-                audioElement.crossOrigin = "anonymous";
-                audioElement.src = URL.createObjectURL(blob);
-                if (trackCtl) {
-                    progressNow(audioElement, times, true);
-                    playBtnControl(audioElement, true);
-                } else {
-                    progressNow(audioElement, times);
-                }
-                
-            } catch (error) {
-                console.log(error);
-            }
-
-        })();
+    if (times > 1) {
+        times = 0;
+        audioElement.pause();
+        URL.revokeObjectURL(audioElement.src);
+        clearInterval(progressInterval);
+        clearInterval(progressBarInterval);
+        audioElement.src = "";
     }
+    
+    (async () => {
+        times += 1;
+        URL.revokeObjectURL(audioElement.src);
+        try {
+
+            let blob = new Blob([ await file.arrayBuffer()], { type: file.type });
+            audioElement.crossOrigin = "anonymous";
+            audioElement.src = URL.createObjectURL(blob);
+            if (trackCtl) {
+                progressNow(audioElement, times, true);
+                playBtnControl(audioElement, true);
+            } else {
+                progressNow(audioElement, times);
+            }
+            
+        } catch (error) {
+            console.log(error);
+        }
+
+    })();
 }
 
 export default audio;
