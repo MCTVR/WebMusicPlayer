42fdfd58314dacb098898891794db96a1b555db9
MCTVR <69135058+MCTVR@users.noreply.github.com>
2022-04-04 22:53:42 +0800

    v1.0.2 Release

--- docs/js/ui.js
+++ docs/js/ui.js
@@ -4,6 +4,10 @@ const playBtn = document.querySelector("div#play-icon");
 const playBtnImg = document.querySelector("div#play-icon").querySelector("img");
 const nextBtn = document.querySelector("div#forward-icon").querySelector("img");
 const prevBtn = document.querySelector("div#backward-icon").querySelector("img");
+const musicControlsDivs = document.querySelectorAll("div.music-controls-divs");
+const autoPlayDiv = document.querySelector("div#autoplay-div");
+const loopDiv = document.querySelector("div#loop-div");
+const shuffleDiv = document.querySelector("div#shuffle-div");
 const progressNowSpan = document.querySelector("span#progress-now-span");
 const progressDurationSpan = document.querySelector("span#progress-duration-span");
 const progressBar = document.querySelector("div.progress-bar");
@@ -119,6 +123,48 @@ function buildTrack(file, id, imgSrc, trackTitle, trackArtist) {
     });
 }
 
+musicControlsDivs.forEach(div => {
+    div.addEventListener("mouseover", () => {
+        anime.remove(div);
+        anime({
+            targets: div,
+            scale: 1.01,
+            duration: 600,
+            elasticity: 400,
+        });
+    });
+    div.addEventListener("mouseleave", () => {
+        anime.remove(div);
+        anime({
+            targets: div,
+            scale: 1,
+            duration: 600,
+            elasticity: 400,
+        });
+    });
+    div.addEventListener("mousedown", () => {
+        anime.remove(div);
+        anime({
+            targets: div,
+            scale: 0.98,
+            duration: 600,
+            elasticity: 400,
+        });
+    });
+    div.addEventListener("mouseup", () => {
+        anime.remove(div);
+        anime({
+            targets: div,
+            scale: 1,
+            duration: 600,
+            elasticity: 400,
+        });
+    });
+    div.addEventListener("click", () => {
+        div.classList.toggle("active-div");
+    });
+});
+
 playBtn.addEventListener("mouseover", () => {
     anime({
         targets: playBtn,
@@ -156,12 +202,12 @@ playBtn.addEventListener("mouseup", () => {
 });
 
 function playAudio(audioElement) {
-    playBtnImg.src = "assets/pause-fill.svg";
+    playBtnImg.src = "assets/icons/pause-fill.svg";
     audioElement.play();
 }
 
 function pauseAudio(audioElement) {
-    playBtnImg.src = "assets/play-fill.svg";
+    playBtnImg.src = "assets/icons/play-fill.svg";
     audioElement.pause();
 }
 
@@ -237,7 +283,17 @@ function trackControl(trackElement) {
 }
 
 function progressNow(audioElement, times, isList=false) {
-    
+    if (isList) {
+        autoPlayDiv.classList.add("active-div");
+        autoPlayDiv.style.display = "flex";
+        loopDiv.style.display = "none";
+        shuffleDiv.style.display = "flex";
+    } else {
+        loopDiv.style.display = "flex";
+        autoPlayDiv.style.display = "flex";
+        autoPlayDiv.style.opacity = "0";
+    }
+
     if (times > 1) {
         try {
             clearInterval(progressInterval);
@@ -290,15 +346,30 @@ function progressNow(audioElement, times, isList=false) {
         progressBar.style.width = progressPercentageInStr;
 
         if (timeNow >= audioElement.duration) {
-            if (isList) {
-                nextBtn.click();
+            const autoPlayDivClass = autoPlayDiv.classList;
+            const shuffleDivClass = shuffleDiv.classList;
+            const loopDivClass = loopDiv.classList;
+            if (isList && autoPlayDivClass.contains("active-div")) {
+                if (shuffleDivClass.contains("active-div")) {
+                    let randomMusicTitleID = Math.floor(Math.random() * musicInputFile.files.length);
+                    let randomTrackElement = document.querySelector(`div#track-${randomMusicTitleID}`);
+                    trackControl(randomTrackElement);
+                } else {
+                    nextBtn.click();
+                }
+            } else if (!isList && loopDivClass.contains("active-div")) {
+                audioElement.currentTime = 0;
+                clearInterval(progressInterval);
+                clearInterval(progressBarInterval);
+                playAudio(audioElement);
+                progressNow(audioElement);
             } else {
-                playBtnImg.src = "assets/play-fill.svg";
+                playBtnImg.src = "assets/icons/play-fill.svg";
+                clearInterval(progressInterval);
+                clearInterval(progressBarInterval);
                 playBtn.addEventListener("click", () => {
-                    clearInterval(progressInterval);
-                    clearInterval(progressBarInterval);
                     playAudio(audioElement);
-                    progressNow(audioElement);
+                    progressNow(audioElement, isList=true);
                 });
             }
         }
